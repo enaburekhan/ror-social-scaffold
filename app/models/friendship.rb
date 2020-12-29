9ffa8b09087a2cca43c85cfd5b114637a09d651c
Gzim <gzimasanni@gmail.com>
2020-12-29 02:28:53 +0100

    Add integration tests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,7 +3,9 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   def accept_request
-    Friendship.create(friend_id: user_id, user_id: friend_id, confirmed: true)
-    update(confirmed: true)
+    self.update_attributes(confirmed: true)
+    Friendship.create!(friend_id: self.user_id,
+                  user_id: self.friend_id,
+                  confirmed: true)
   end
 end
